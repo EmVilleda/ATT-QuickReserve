173bdc01ec49cbb9aeb5a95c6d06aef99d62cd22
Toscano, Andrew (at892q) <at892q@us.att.com>
2014-07-02 11:17:26 -0500

    ...

--- QuickReserve/app/src/main/java/quickreserve/app/DateTimeActivity.java
+++ QuickReserve/app/src/main/java/quickreserve/app/DateTimeActivity.java
@@ -87,7 +87,6 @@ public class DateTimeActivity extends Activity {
 
                 int date = (year * 10000) + ((month+1)*100) + dayOfMonth;
                 tempDate = TimeParser.parseDateFormat(date);
-                Toast.makeText(DateTimeActivity.this, "" + tempDate, Toast.LENGTH_SHORT).show();
 
                 Log.e("test", "onDateChanged");
                 Log.e("test","button: " + mSelectedDate.getText());
